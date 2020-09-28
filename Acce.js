3d9076b883e5d9700350ee6ae52fb4dca759b039
furukami <soranguriarumika@gmail.com>
2020-09-28 21:23:48 +0900

    Test

---
+++ Acce.js
@@ -0,0 +1,13 @@
+window.addEventListener('deviceorientation', handleOrientation, true);
+
+function handleOrientation(event) {
+    const absolute = event.absolute;
+    const alpha = event.alpha;
+    const beta = event.beta;
+    const gamma = event.gamma;
+
+    const hoge = document.getElementById("hoge");
+
+    hoge.textContent = absolute
+    
+}
